1df2a3802b8c165c8684108356097299e9110023
SzBalesz <baleszvagyok7000@gmail.com>
2026-09-16 01:24:59 +0200

    Bugfix

--- src/hooks/useLanyard.tsx
+++ src/hooks/useLanyard.tsx
@@ -10,7 +10,7 @@ export function LanyardProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
     const fetchStatus = async () => {
       try {
-        const res = await fetch("https://api.lanyard.rest/v1/users/305732881144086528", { 
+        const res = await fetch(`https://api.lanyard.rest/v1/users/305732881144086528?_t=${Date.now()}`, { 
           cache: "no-store" 
         });
         const json = await res.json();
